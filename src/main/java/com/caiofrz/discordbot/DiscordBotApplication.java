a78ccd615399b113b6a76dd2e84d397c99650c27
Caio Ferraz <caioferrazalmeida.27@gmail.com>
2024-03-30 22:54:51 -0300

    initial commit

---
+++ src/main/java/com/caiofrz/discordbot/DiscordBotApplication.java
@@ -0,0 +1,13 @@
+package com.caiofrz.discordbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscordBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscordBotApplication.class, args);
+	}
+
+}
